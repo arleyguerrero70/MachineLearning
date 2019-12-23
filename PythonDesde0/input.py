ac20a42f02c19741e258d16bef169d9145b1dd42
Sanluar <proyectosenasanluar@gmail.com>
2019-12-22 22:00:52 -0500

    Primer avance de estudio

---
+++ PythonDesde0/input.py
@@ -0,0 +1,2 @@
+nombre = input("por favor dime tu nombre \n")
+print("Hola " + nombre + "! ¿Como estás?")
---
+++ PythonDesde0/variables.py
@@ -0,0 +1,9 @@
+nombre_alumno = "Arley"
+edad_alumno = 23
+peso = 55.70
+inteligente = True
+
+print(nombre_alumno)
+print(edad_alumno)
+print(peso)
+print(inteligente)
